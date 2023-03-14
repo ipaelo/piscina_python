95b7bf694f4df4924abd195ec8cc1d215bed8f17
itorres- <yosoy@ipaelo.com>
2023-03-14 12:11:12 +0100

    pppepe

--- Module_00/ex02/whois.py
+++ Module_00/ex02/whois.py
@@ -7,15 +7,15 @@ if len(sys.argv) > 1:
     if number[0] == "-":
         number.lstrip("-")
     if len(sys.argv) == 2 and number.isdigit():
-        if int(sys.argv[1]) == 0:
+        if int(number) == 0:
             print("Es un cero")
-        elif int(sys.argv[1]) % 2 == 0:
+        elif int(number) % 2 == 0:
             print("Esto es par")
         else:
             print("Es impar")
     elif len(sys.argv) > 2:
         print("AssertionError: more than one argument are provided")
-    elif not sys.argv[1].isdigit():
+    elif not number.isdigit():
         print("AssertionError: argument is not an integer")
 
 
