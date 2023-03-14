dfd9caf92bc576c6f53f8a462dc91e146b8a8f17
itorres- <yosoy@ipaelo.com>
2023-03-14 12:10:25 +0100

    subidas

--- Module_00/ex03/count.py
+++ Module_00/ex03/count.py
@@ -1,10 +1,24 @@
 import sys
+import string   
 
-
-if len(sys.argv) < 1:
-    cadena=input()
-elif len(sys.argv) > 1:
-    cadena=" ".join(sys.argv([1:])
-if not isinstance(cadena,str)
-    print("Error: No es una cadena"
-            print("Error: No es una cadena"))
+def text_analyzer(cadena = None):
+    """Esta función cuenta caracteres, mayúsculas, minúsculas, signos de puntuación  y espacios."""
+    if cadena == None:
+        print("¿Qué texto quieres analizar") 
+        cadena = input()          
+    if not isinstance(cadena, str): 
+        print("Error: No es una cadena")
+    else: 
+        print("Este texto tiene ",len(cadena)," caracteres\n")
+        print("-", len([c for c in cadena if c.isupper()]), " mayúsculas\n") 
+        print("-", len([c for c in cadena if c.islower()]), " minúsculas\n") 
+        print("-", len([c for c in cadena if c.isspace()]), " espacios\n")
+        i = 0
+        for c in cadena:
+         if c in string.punctuation:
+                i = i+1
+        print("-", i, " son signos de puntuación\n") 
+         
+if __name__ == "__main__":
+        cadena = sys.argv[1]
+        text_analyzer(cadena)
